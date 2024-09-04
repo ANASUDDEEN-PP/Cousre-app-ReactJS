965089f0b5398cfc1a2045d27d45aa24c17e773d
ana.su._ <98901363+ANASUDDEEN-PP@users.noreply.github.com>
2024-09-04 13:42:26 +0530

    Create listCourse.jsx
    13:41 - 04 september 2024
    Create a new page and type a starting codes

---
+++ course-app-reactjs/src/Components/Admin/listCourse.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './admin.css'
+
+//import modules
+import NavBar from './navBar'
+
+export default class listCourse extends Component {
+  render() {
+    return (
+      <div className='crs-main-sct'>
+        <NavBar/>
+        list course
+      </div>
+    )
+  }
+}
